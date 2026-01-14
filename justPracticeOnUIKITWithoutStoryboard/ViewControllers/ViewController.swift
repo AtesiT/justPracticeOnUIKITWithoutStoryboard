4392745da8d45636af26c9a05ce95f71f8d106a0
AtesiT <atesit@mail.ru>
2026-01-14 22:54:02 +0300

    ADDED: saveButton and the button placed on the view controller

--- justPracticeOnUIKITWithoutStoryboard/ViewControllers/ViewController.swift
+++ justPracticeOnUIKITWithoutStoryboard/ViewControllers/ViewController.swift
@@ -2,11 +2,34 @@ import UIKit
 
 final class ViewController: UIViewController {
 
+    private lazy var saveButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .green
+        button.tintColor = .white
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        view.addSubview(saveButton)
+        setupConstraints()
     }
 
 
 }
 
+extension ViewController {
+    func setupConstraints() {
+        NSLayoutConstraint.activate([
+            saveButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
+            saveButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            saveButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+        ])
+    }
+}
+
+#Preview {
+    ViewController()
+}
