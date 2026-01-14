5bea770f43d2e4956660bc76b69bd84596e07559
AtesiT <atesit@mail.ru>
2026-01-14 23:05:45 +0300

    ADDED: label under saveButton

--- justPracticeOnUIKITWithoutStoryboard/ViewControllers/ViewController.swift
+++ justPracticeOnUIKITWithoutStoryboard/ViewControllers/ViewController.swift
@@ -10,14 +10,26 @@ final class ViewController: UIViewController {
         return button
     }()
     
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hello"
+        label.textAlignment = .center
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        view.addSubview(saveButton)
+        setSubViews(subView: saveButton, titleLabel)
         setupConstraints()
     }
 
-
+    private func setSubViews(subView: UIView...) {
+        subView.forEach { theView in
+            view.addSubview(theView)
+        }
+    }
 }
 
 extension ViewController {
@@ -25,7 +37,11 @@ extension ViewController {
         NSLayoutConstraint.activate([
             saveButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 100),
             saveButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
-            saveButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+            saveButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            titleLabel.topAnchor.constraint(equalTo: saveButton.topAnchor, constant: 50),
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
         ])
     }
 }
