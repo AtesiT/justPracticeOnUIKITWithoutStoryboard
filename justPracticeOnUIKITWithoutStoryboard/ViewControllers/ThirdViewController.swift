48a0f638ad8b0f7a248e2287edfc42f309569b6c
AtesiT <atesit@mail.ru>
2026-01-30 23:06:27 +0300

    MADE: init empty ThirdViewController

---
+++ justPracticeOnUIKITWithoutStoryboard/ViewControllers/ThirdViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+final class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+}
+
+#Preview {
+    ThirdViewController()
+}
