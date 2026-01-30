f0dcf8049f33ec7e65a759eaa4b9d686ece5f0bb
AtesiT <atesit@mail.ru>
2026-01-30 23:23:38 +0300

    MADE: added greenView (looks like a green rectangle)

--- justPracticeOnUIKITWithoutStoryboard/ViewControllers/ThirdViewController.swift
+++ justPracticeOnUIKITWithoutStoryboard/ViewControllers/ThirdViewController.swift
@@ -2,9 +2,29 @@ import UIKit
 
 final class ThirdViewController: UIViewController {
     
+    private lazy var theView: UIView = {
+        let theView = UIView()
+        theView.backgroundColor = .green
+        theView.translatesAutoresizingMaskIntoConstraints = false
+        return theView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
+        view.addSubview(theView)
+        setupConstraints()
+    }
+}
+
+extension ThirdViewController {
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            theView.topAnchor.constraint(equalTo: view.topAnchor, constant: 20),
+            theView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            theView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            theView.bottomAnchor.constraint(equalTo: view.topAnchor, constant: 40)
+            ])
     }
 }
 
