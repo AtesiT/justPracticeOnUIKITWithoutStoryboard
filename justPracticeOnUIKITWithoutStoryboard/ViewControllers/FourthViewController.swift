b856d64d3a7af53f40eb9c48fcb5b7d627f7301d
AtesiT <atesit@mail.ru>
2026-04-06 23:56:06 +0300

    ADD: RedButton on FourthVC

--- justPracticeOnUIKITWithoutStoryboard/ViewControllers/FourthViewController.swift
+++ justPracticeOnUIKITWithoutStoryboard/ViewControllers/FourthViewController.swift
@@ -10,9 +10,18 @@ final class FourthViewController: UIViewController {
         return button
     }()
     
+    private lazy var redButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Button", for: .normal)
+        button.setTitleColor(.red, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.addSubview(greenButton)
+        view.addSubview(redButton)
         makeConstraints()
     }
 }
@@ -25,7 +34,9 @@ extension FourthViewController {
                 greenButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
                 greenButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -100),
                 
-                
+                redButton.topAnchor.constraint(equalTo: greenButton.topAnchor, constant: 60),
+                redButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
+                redButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -100),
             ]
         )
     }
