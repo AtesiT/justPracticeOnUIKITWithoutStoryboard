d2452cf890130ebf70fbfc6a670550cdfb5cd6db
AtesiT <atesit@mail.ru>
2026-04-07 23:25:01 +0300

    ADD: blue button on FourthVC

--- justPracticeOnUIKITWithoutStoryboard/ViewControllers/FourthViewController.swift
+++ justPracticeOnUIKITWithoutStoryboard/ViewControllers/FourthViewController.swift
@@ -20,9 +20,18 @@ final class FourthViewController: UIViewController {
         return button
     }()
     
+    private lazy var blueButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Button", for: .normal)
+        button.backgroundColor = .blue
+        button.setTitleColor(.white, for: .normal)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setSubviews(theView: greenButton, redButton)
+        setSubviews(theView: greenButton, redButton, blueButton)
         makeConstraints()
     }
     
@@ -44,6 +53,10 @@ extension FourthViewController {
                 redButton.topAnchor.constraint(equalTo: greenButton.topAnchor, constant: 60),
                 redButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
                 redButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -100),
+                
+                blueButton.topAnchor.constraint(equalTo: redButton.topAnchor, constant: 60),
+                blueButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
+                blueButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -100),
             ]
         )
     }
