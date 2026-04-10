7bdc98f63797e7198ce9b2aa9990d51930c4c7b6
AtesiT <atesit@mail.ru>
2026-04-10 23:28:14 +0300

    ADD: Button on FourthVC (that will display general clicks (also colors) in print)

--- justPracticeOnUIKITWithoutStoryboard/ViewControllers/FourthViewController.swift
+++ justPracticeOnUIKITWithoutStoryboard/ViewControllers/FourthViewController.swift
@@ -36,9 +36,19 @@ final class FourthViewController: UIViewController {
         return button
     }()
     
+    private lazy var generalCountButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.setTitle("Check count", for: .normal)
+        button.backgroundColor = .black
+        button.setTitleColor(.white, for: .normal)
+        button.addTarget(self, action: #selector(generalCountClicked), for: .touchUpInside)
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        setSubviews(theView: greenButton, redButton, blueButton)
+        setSubviews(theView: greenButton, redButton, blueButton, generalCountButton)
         makeConstraints()
     }
     
@@ -63,8 +73,8 @@ final class FourthViewController: UIViewController {
         print(countClickedBlueButton)
     }
     
-    private func generalCountClicked() {
-        print("General clicks: \(countClickedRedButton + countClickedBlueButton + countClickedGreenButton). \n Clicks on green button: \(countClickedGreenButton),\n Clicks on red button: \(countClickedRedButton), \n Clicks on blue button: \(countClickedBlueButton)")
+    @objc private func generalCountClicked() {
+        print("General clicks: \(countClickedRedButton + countClickedBlueButton + countClickedGreenButton). \n Clicks on green button: \(countClickedGreenButton),\n Clicks on red button: \(countClickedRedButton), \n Clicks on blue button: \(countClickedBlueButton).")
     }
 }
 
@@ -83,6 +93,10 @@ extension FourthViewController {
                 blueButton.topAnchor.constraint(equalTo: redButton.topAnchor, constant: 60),
                 blueButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
                 blueButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -100),
+                
+                generalCountButton.topAnchor.constraint(equalTo: blueButton.topAnchor, constant: 60),
+                generalCountButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 100),
+                generalCountButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -100),
             ]
         )
     }
