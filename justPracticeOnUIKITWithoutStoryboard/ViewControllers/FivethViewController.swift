282e8e8f900d23499ac5b4cf4034bb1cd8a608a1
AtesiT <atesit@mail.ru>
2026-05-08 18:41:52 +0300

    ADD: GreenView in bottom of scrollView (it has been made to display an ability scroll the screen)

--- justPracticeOnUIKITWithoutStoryboard/ViewControllers/FivethViewController.swift
+++ justPracticeOnUIKITWithoutStoryboard/ViewControllers/FivethViewController.swift
@@ -8,10 +8,21 @@ final class FivethViewController: UIViewController {
         return scrollview
     }()
     
+    private lazy var contentView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .green
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
         view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        
+        setConstraints()
     }
 }
 
@@ -22,6 +33,25 @@ extension FivethViewController {
             scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
             scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
             scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            
+            //  MARK: - ContentView in ScrollView
+            //  Устанавливаем верхний край квадрата в 1000 points от начала нашего scrollView
+            contentView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor, constant: 1000),
+            
+            //  Привязываем левый и правый края нашего ContentView к правому и левому краям ScrollView
+            contentView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
+            
+            //  Устанавливаем ширину квадрата с шириной равной нашему ScrollView
+            contentView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor),
+            
+            //  Ширина нашего contentView
+            contentView.heightAnchor.constraint(equalToConstant: 100),
+            
+            //  Устанавливаем нижний край нашего ContentView к нижнему краю ScrollView
+            contentView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor)
+            //  MARK: - END OF ContentView
         ])
     }
 }
