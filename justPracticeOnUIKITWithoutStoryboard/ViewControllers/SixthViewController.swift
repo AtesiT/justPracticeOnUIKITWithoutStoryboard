bebe8569b07b47526166aa75dd5419f03138d241
AtesiT <atesit@mail.ru>
2026-05-14 21:58:10 +0300

    ADD: setLabel on SixthVC

--- justPracticeOnUIKITWithoutStoryboard/ViewControllers/SixthViewController.swift
+++ justPracticeOnUIKITWithoutStoryboard/ViewControllers/SixthViewController.swift
@@ -10,10 +10,19 @@ final class SixthViewController: UIViewController {
         return button
     }()
     
+    private lazy var setLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Test"
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
         view.addSubview(setButton)
+        view.addSubview(setLabel)
         setConstraints()
     }
 }
@@ -23,8 +32,12 @@ extension SixthViewController {
         NSLayoutConstraint.activate(
             [
             setButton.topAnchor.constraint(equalTo: view.topAnchor, constant: 60),
-            setButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 60),
-            setButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -60),
+            setButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 60),
+            setButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -60),
+            
+            setLabel.topAnchor.constraint(equalTo: setButton.topAnchor, constant: 60),
+            setLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 60),
+            setLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -60),
             ]
         )
     }
