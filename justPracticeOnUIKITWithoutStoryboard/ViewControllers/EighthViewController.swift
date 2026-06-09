aaa976105c6cd84f7022d2f2fad831b8aff3cc60
AtesiT <atesit@mail.ru>
2026-06-09 23:46:26 +0300

    ADD: EighthVC

---
+++ justPracticeOnUIKITWithoutStoryboard/ViewControllers/EighthViewController.swift
@@ -0,0 +1,13 @@
+import UIKit
+
+final class EighthViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
+#Preview {
+    EighthViewController()
+}
