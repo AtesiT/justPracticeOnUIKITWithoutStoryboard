aafaa1e0f809202ff0b02a3ba6a1a899fa4a0c64
AtesiT <atesit@mail.ru>
2026-06-09 23:56:02 +0300

    ADD: scrollView with content in EighthVC

--- justPracticeOnUIKITWithoutStoryboard/ViewControllers/EighthViewController.swift
+++ justPracticeOnUIKITWithoutStoryboard/ViewControllers/EighthViewController.swift
@@ -2,12 +2,44 @@ import UIKit
 
 final class EighthViewController: UIViewController {
     
+    private let scrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        return scrollView
+    }()
+    
+    private let contentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        view.addSubview(scrollView)
+        view.addSubview(contentView)
+        setConstraints()
     }
 }
 
 #Preview {
     EighthViewController()
+}
+
+extension EighthViewController {
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            
+            contentView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.contentLayoutGuide.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor),
+            
+            contentView.widthAnchor.constraint(equalTo: scrollView.frameLayoutGuide.widthAnchor),
+        ])
+    }
 }
